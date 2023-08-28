53b67062caa05a7f79dd6758de3580773f9bafcb
sergei-s <sergei.shalimoff@gmail.com>
2023-08-28 22:26:08 +0500

    ProjectPaths.BASE_LOG_CONFIG added;
    modified file:
    config/general.py

--- config/general.py
+++ config/general.py
@@ -6,3 +6,4 @@ class ProjectPaths:
     CONFIG = Path(__file__).parent
     ROOT = CONFIG.parent
     LOGS = os.path.join(ROOT, 'logs')
+    BASE_LOG_CONFIG = os.path.join(LOGS, "logging_config.ini")
